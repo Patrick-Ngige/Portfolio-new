803bb7d085cc603599d241afad7522cab88d9b87
Patrick-Ngige <wakemanjajr@gmail.com>
2024-07-25 01:04:48 +0300

    navbar working

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ const navClose = document.getElementById('nav-close');
             navMenu.classList.remove('show-menu');
         })
     }
+
+    //REMOVE MOBILE MENU
+
+    const navLink = document.querySelectorAll('.nav__link');
+
+    const linkAction = () => {
+        const navMenu = document.getElementById('nav-menu');
+
+        // click nav__link to remove show menu class 
+        navMenu.classList.remove('show-menu');
+    }
+
+    navLink.forEach(n => n.addEventListener('click', linkAction));
