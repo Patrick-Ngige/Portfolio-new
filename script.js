60e55cc5b6b7bc8c21b8df698b19a90f0b9c23db
Patrick-Ngige <wakemanjajr@gmail.com>
2024-07-27 18:11:11 +0300

    dark mode functionality + styling

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { API_KEY, serviceID } from "./config";
+// import { API_KEY, serviceID } from "./config";
 
    // SHOWING HIDDEN NAV 
 
@@ -13,7 +13,9 @@ const navClose = document.getElementById('nav-close');
         navToggle.addEventListener('click', () => {
             navMenu.classList.add('show-menu');
         })
-    } else if ( navClose ) {
+    } 
+    
+    if ( navClose ) {
         navClose.addEventListener('click', () => {
             navMenu.classList.remove('show-menu');
         })
@@ -36,7 +38,7 @@ const navClose = document.getElementById('nav-close');
     const shadowHeader = () => {
         const header = document.getElementById('header');
 
-        this.scrollY >= 50 ? header.classList.add('shadow-header')
+        window.scrollY >= 50 ? header.classList.add('shadow-header')
                            : header.classList.remove('shadow-header');
     }
 
@@ -53,7 +55,7 @@ const navClose = document.getElementById('nav-close');
         e.preventDefault();
 
         // serviceID - templateID - #form - publicKey
-        emailjs.sendForm(serviceID,'template_3ypmkyb','#contact-form', API_KEY)
+        emailjs.sendForm('service_0gu7r8v','template_3ypmkyb','#contact-form', 'Q0tB-gMsrTiO_0G27')
         .then(() => {
             //show sent message if successful
             contactMessage.textContent = 'Message sent successfully ✅';
@@ -82,7 +84,7 @@ const navClose = document.getElementById('nav-close');
         const scrollUp = document.getElementById('scroll-up')
 
         //when the scroll is higher than the 350 viewport height, add the 
-        this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        window.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
                             : scrollUp.classList.remove('show-scroll');
     }
     
@@ -91,7 +93,7 @@ const navClose = document.getElementById('nav-close');
 
 //  ========== SCROLL SECTIONS ACTIVE LINK =============
 
-const sections = decument.querySelectorAll('section[id]');
+const sections = document.querySelectorAll('section[id]');
 
 
 const scrollActive = () => {
@@ -112,4 +114,36 @@ const scrollActive = () => {
 }
 
 window.addEventListener('scroll', scrollActive);
-    
+
+
+// ============== DARK LIGHT THEME ============== 
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+//previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+//obtain current theme that the interface has by validating the dark-theme class
+const getCurrentTheme  = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+//validating if the user previously chose a topic
+if (selectedTheme) {
+    //asking the issue to know if the dark theme is activated or deactivated
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+
+}
+
+//activating/deactivating the theme using the button
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    //saving the current theme + icon
+    localStorage.setItem('selected-theme', getCurrentTheme)
+    localStorage.setItem('selected-icon', getCurrentIcon)
+})
