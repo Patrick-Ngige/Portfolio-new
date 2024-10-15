2d9acc046ab9bd4201111d43b3502e4e1518a90a
Patrick-Ngige <wakemanjajr@gmail.com>
2024-10-16 00:35:31 +0300

    adding skill badges on the about us section

--- script.js
+++ script.js
@@ -146,3 +146,15 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon)
 })
 
+
+// $$$$$$$$$$$$$$$$ SKILLS 
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
